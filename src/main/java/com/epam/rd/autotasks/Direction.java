af20ee637f6b96f3a5b4853356a5cae940e4dd7a
MiyamizuTakaki <mitsuha1@outlook.com>
2022-08-03 18:34:55 +0300

    1834

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -8,11 +8,43 @@ public enum Direction {
     Direction(final int degrees) {
         this.degrees = degrees;
     }
-
+    public final static int  MIN_AGX = 0;
+    public final static int  MAX_AGX = 360;
     private int degrees;
-
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        try{
+            while(degrees<0||degrees>=360)
+            {
+                if(degrees<0)
+                    degrees = degrees+360;
+                else if(degrees>=360)
+                    degrees = degrees-360;
+            }
+            switch (degrees){
+                case 0:
+                    return N;
+                case 45:
+                    return NE;
+                case 90:
+                    return E;
+                case 135:
+                    return SE;
+                case 180:
+                    return S;
+                case 225:
+                    return SW;
+                case 270:
+                    return W;
+                case 315:
+                    return NW;
+                default:
+                    return null;
+            }
+        }catch (Exception e)
+        {
+            throw new UnsupportedOperationException();
+        }
+
     }
 
     public static Direction closestToDegrees(int degrees) {
